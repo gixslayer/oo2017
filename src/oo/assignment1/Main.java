8e94ce5a030dbd2f3f75c45e3bffb5732f660e35
Ciske H <ciske_harsema_2@hotmail.com>
2018-02-06 16:57:55 +0100

    Initial commit

---
+++ src/oo/assignment1/Main.java
@@ -0,0 +1,11 @@
+package oo.assignment1;
+
+/**
+ * @author Ciske Harsema - s1010048
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
